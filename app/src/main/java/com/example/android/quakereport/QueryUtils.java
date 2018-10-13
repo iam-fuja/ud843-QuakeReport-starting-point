ad9207cbb5527ba0349df40fb973cd7d8c316926
Fuja <makanjuolao@gmail.com>
2018-10-13 02:31:19 +0100

    Loader update

--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -177,6 +177,7 @@ public final class QueryUtils {
      * Query the USGS dataset and return a list of {@link Earthquake} objects.
      */
     public static List<Earthquake> fetchEarthquakeData(String requestUrl) {
+        Log.i(LOG_TAG, "TEST: fetchEarthquakeData() called ... ");
         // Create URL object
         URL url = createUrl(requestUrl);
 
